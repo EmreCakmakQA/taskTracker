ffd5677a4fe44161f8a1599b50120a8c8b8d3e62
EmreCakmakQA <ECakmak@qa.com>
2021-02-19 16:12:15 +0000

    Add Tasks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dentist App.',
+      day: 'Feb 5th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Dentist App.',
+      day: 'Feb 7th at 3:30pm',
+      reminder: false,
+    }
+  ])
+
   return (
     <div className="container">
-      <Header title="Task Tracker" />
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,31 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
+
+const Button = ({ color, text, onClick }) => {
+
+
     return (
-        <button style={{ backgroundColor: color }} className='btn'>{text}</button>
+        <button
+            onClick={onClick}
+            style={{ backgroundColor: color }}
+            className='btn'>{text}
+        </button>
     )
 }
 
+
+
+
+// Default props
 Button.defaultProps = {
     color: 'steelblue'
 }
 
+// Default proptypes
 Button.propTypes = {
     text: PropTypes.string,
-    color: PropTypes.string
+    color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 
