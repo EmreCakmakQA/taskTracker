f052bf644f773bf654062e17e49b89ee5a166cec
EmreCakmakQA <ECakmak@qa.com>
2021-02-19 15:53:37 +0000

    Add button component with default proptypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+
 const Header = ({ title }) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' />
         </header>
     )
+}
+
+// Defaults title to 'Task Tracker'
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+// Restricts the prop type to a String
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
